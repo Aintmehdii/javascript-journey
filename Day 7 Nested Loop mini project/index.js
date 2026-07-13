44ea059a9bbe0e5227004e96fbd3b758a4bbd219
Mahdi <omohammadi376@gmail.com>
2026-07-13 11:31:53 +0330

    Initialize FutureAcademy data

---
+++ Day 7 Nested Loop mini project/index.js
@@ -0,0 +1,349 @@
+const FutureAcademy = [
+    {
+        Classname: 'Math',
+        Classteacher: 'Mr. Martinez',
+        Floornumber: 'first floor',
+            students : [
+                {
+                    Name: 'Jack',
+                    Age: '21',
+                    Country: 'England',
+                    Grade: 85,
+                    hobbies : [
+                        'Football','Coding','Tennis'
+                    ]
+                },
+                {
+                    Name: 'Hellen',
+                    Age: '20',
+                    Country: 'England',
+                    Grade: 100,
+                    hobbies : [
+                        'Gaming','Streaming','Editing'
+                    ]
+                },
+                {
+                    Name: 'Sebastian',
+                    Age: '18',
+                    Country: 'England',
+                    Grade: 45,
+                    hobbies : [
+                        'Football','Reading','Tennis'
+                    ]
+                },
+                {
+                    Name: 'Mahmoud',
+                    Age: '19',
+                    Country: 'Egypt',
+                    Grade: 100,
+                    hobbies : [
+                        'Swiming','Coding','Running'
+                    ]
+                }
+            ],
+              Subjects : [
+                    {
+                        Subjectname: 'Algebra',
+                        Teacher: 'Mr. Martinez',
+                        Weeklyhours: 5,
+                         exam : [
+                            {
+                                Examname: 'A75G',
+                                Maximumscore: 100
+                            },
+                            {
+                                Examname: 'A76G',
+                                Maximumscore: 85
+                            },
+                            {
+                                Examname: 'A77G',
+                                Maximumscore: 65
+                            }
+                         ]
+                    },
+                    {
+                        Subjectname: 'Geometry',
+                        Teacher: 'Mr. Martinez',
+                        Weeklyhours: 7,
+                        exam : [
+                            {
+                                Examname: 'Ge17',
+                                Maximumscore: 100
+                            },
+                            {
+                                Examname: 'Ge18',
+                                Maximumscore: 100
+                            },
+                            {
+                                Examname: 'Ge19',
+                                Maximumscore: 100
+                            }
+                         ]
+                    },
+                    {
+                        Subjectname: 'Logic',
+                        Teacher: 'Mr. Martinez',
+                        Weeklyhours: 5,
+                        exam : [
+                            {
+                                Examname: 'LopA',
+                                Maximumscore: 85
+                            },
+                            {
+                                Examname: 'LopB',
+                                Maximumscore: 90
+                            },
+                            {
+                                Examname: 'LopC',
+                                Maximumscore: 95
+                            }
+                         ]
+                    },
+                 ]
+                 },  
+                 {
+                 Classname: 'Sport',
+                 Classteacher: 'Mrs. Sara and Mr. Sebastian',
+                 Floornumber: 'first floor',
+                 students : [
+                 {
+                    Name: 'Jack',
+                    Age: '21',
+                    Country: 'England',
+                    Grade: 100,
+                    hobbies : [
+                        'Football','Coding','Tennis'
+                    ]
+                 },
+                 {
+                    Name: 'Martineli',
+                    Age: '22',
+                    Country: 'Afganistan',
+                    Grade: 100,
+                    hobbies : [
+                        'Tennis','Swiming','Reading'
+                    ]
+                 },
+                 {
+                    Name: 'Ricardo',
+                    Age: '22',
+                    Country: 'England',
+                    Grade: 100,
+                    hobbies : [
+                        'Football','Basketball','Coding'
+                    ]
+                 },
+                 {
+                    Name: 'Mahmoud',
+                    Age: '19',
+                    Country: 'Egypt',
+                    Grade: 100,
+                    hobbies : [
+                        'Swiming','Coding','Running'
+                    ]
+                 }
+                 ],
+                 Subjects : [
+                  {
+                      Subjectname: 'Performance',
+                      Teacher: 'Mrs. Sara',
+                      Weeklyhours: 2,
+                       exam : [
+                          {
+                              Examname: 'PE8',
+                              Maximumscore: 50
+                          },
+                          {
+                              Examname: 'PE9',
+                              Maximumscore: 55
+                          },
+                          {
+                              Examname: 'PE10',
+                              Maximumscore: 50
+                          }
+                       ]
+                  },
+                  {
+                      Subjectname: 'Foundations of biomechanics',
+                      Teacher: 'Mrs. Sara',
+                      Weeklyhours: 5,
+                      exam : [
+                          {
+                              Examname: 'FOBa',
+                              Maximumscore: 80
+                          },
+                          {
+                              Examname: 'FOBb',
+                              Maximumscore: 80
+                          },
+                          {
+                              Examname: 'FOBc',
+                              Maximumscore: 90
+                          }
+                       ]
+                  },
+                  {
+                      Subjectname: 'Football',
+                      Teacher: 'Mr. Sebastian',
+                      Weeklyhours: 2,
+                      exam : [
+                          {
+                              Examname: 'BA0',
+                              Maximumscore: 50
+                          },
+                          {
+                              Examname: 'BA1',
+                              Maximumscore: 55
+                          },
+                          {
+                              Examname: 'BA2',
+                              Maximumscore: 100
+                          }
+                       ]
+                  },
+                 ]
+                 },
+                 {
+                 Classname: 'French Language',
+                 Classteacher: 'Mr. Jacson and Mrs. Bliagous',
+                 Floornumber: 'third floor',
+                 students : [
+                 {
+                    Name: 'Milino',
+                    Age: '22',
+                    Country: 'Italy',
+                    Grade: 85,
+                    hobbies : [
+                        undefined
+                    ]
+                 },
+                 {
+                    Name: 'Mario',
+                    Age: '24',
+                    Country: 'Italy',
+                    Grade: 45,
+                    hobbies : [
+                        undefined
+                    ]
+                 },
+                 {
+                    Name: 'Ahmed',
+                    Age: '17',
+                    Country: 'Afghanistan',
+                    Grade: 100,
+                    hobbies : [
+                        'Football','Coding','Tennis'
+                    ]
+                 },
+                 {
+                    Name: 'Mahmoud',
+                    Age: '19',
+                    Country: 'Egypt',
+                    Grade: 95,
+                    hobbies : [
+                        'Swiming','Coding','Running'
+                    ]
+                 }
+                 ],
+                 Subjects : [
+                  {
+                      Subjectname: 'Lhistoire',
+                      Teacher: 'Mrs. Bliagous',
+                      Weeklyhours: 5,
+                       exam : [
+                          {
+                              Examname: 'HL01',
+                              Maximumscore: 100
+                          },
+                          {
+                              Examname: 'HL02',
+                              Maximumscore: 100
+                          },
+                          {
+                              Examname: 'HL03',
+                              Maximumscore: 50
+                          }
+                       ]
+                  },
+                  {
+                      Subjectname: 'Lareligion',
+                      Teacher: 'Mr. Jacson',
+                      Weeklyhours: 5,
+                      exam : [
+                          {
+                              Examname: 'LARE10',
+                              Maximumscore: 50
+                          },
+                          {
+                              Examname: 'LARE20',
+                              Maximumscore: 50
+                          },
+                          {
+                              Examname: 'LARE30',
+                              Maximumscore: 50
+                          }
+                       ]
+                  },
+                  {
+                      Subjectname: 'Lart',
+                      Teacher: 'Mrs. Bliagous',
+                      Weeklyhours: 5,
+                      exam : [
+                          {
+                              Examname: 'ARTOS',
+                              Maximumscore: 60
+                          },
+                          {
+                              Examname: 'ARTOSS',
+                              Maximumscore: 60
+                          },
+                          {
+                              Examname: 'ARTOSSS',
+                              Maximumscore: 70
+                          }
+                       ]
+                  },
+        ]
+    }
+] 
+                 
+
+for (let i = 0; i<FutureAcademy.length; i++){
+    console.log(`==========================================
+    
+Class: ${FutureAcademy[i].Classname}
+Teacher: ${FutureAcademy[i].Classteacher}
+Floor: ${FutureAcademy[i].Floornumber}
+
+Sudents
+------------------------`);
+    for (let s = 0; s<FutureAcademy[i].students.length; s++){
+        console.log(`
+${FutureAcademy[i].students[s].Name}
+Age: ${FutureAcademy[i].students[s].Age}
+Country: ${FutureAcademy[i].students[s].Country}
+Grade: ${FutureAcademy[i].students[s].Grade}
+Hobbies: `)
+
+
+        for (let h = 0; h<FutureAcademy[i].students[s].hobbies.length; h++){
+            console.log(` - ${FutureAcademy[i].students[s].hobbies[h]}`)
+        }
+    }
+
+    
+
+    for (let sub = 0; sub <FutureAcademy[i].Subjects.length; sub++){
+        console.log(`
+Subjects 
+------------------------
+Subject name: ${FutureAcademy[i].Subjects[sub].Subjectname}
+Teacher: ${FutureAcademy[i].Subjects[sub].Teacher}
+Weekly Hours: ${FutureAcademy[i].Subjects[sub].Weeklyhours}
+`)
+        for (let exam = 0; exam < FutureAcademy[i].Subjects[sub].exam.length; exam++){
+            console.log(`Exam code: ${FutureAcademy[i].Subjects[sub].exam[exam].Examname}
+Maximum score: ${FutureAcademy[i].Subjects[sub].exam[exam].Maximumscore}`)
+        }
+    }
+}
