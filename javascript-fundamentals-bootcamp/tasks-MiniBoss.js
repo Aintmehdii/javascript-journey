9f5ca901b818e19198982c6091b445c9413cff43
Mahdi <omohammadi376@gmail.com>
2026-08-02 12:05:22 +0330

    Last update generateLibrary database

--- javascript-fundamentals-bootcamp/tasks-MiniBoss.js
+++ javascript-fundamentals-bootcamp/tasks-MiniBoss.js
@@ -54,3 +54,97 @@ console.log(generateSteamReport(players))
 
 
 
+
+
+
+
+// const database = {
+    players: [
+        {
+            username: "Mahdi",
+            premium: true,
+            games: [
+                { title: "CS2", genre: "FPS", free: true },
+                { title: "Elden Ring", genre: "RPG", free: false }
+            ]
+        },
+        {
+            username: "Jack",
+            premium: false,
+            games: [
+                { title: "Minecraft", genre: "Sandbox", free: false },
+                { title: "CS2", genre: "FPS", free: true }
+            ]
+        },
+        {
+            username: "Emma",
+            premium: true,
+            games: [
+                { title: "Valorant", genre: "FPS", free: true },
+                { title: "Cyberpunk", genre: "RPG", free: false }
+            ]
+        }
+    ]
+};
+
+function generateLibrary(database) {
+    const {players} = database;
+    const premiumPlayers = players.filter((player)=>{
+        return player.premium
+    }).map((playername)=>{
+        return playername.username
+    })
+
+    const allGames = players.flatMap((game)=>{
+        return game.games
+    }).map((gamename)=>{
+        return gamename.title
+    })
+    
+    const uniqueGames = [...new Set(
+        players.flatMap((player)=>{
+            return player.games
+        }).map((gametitle)=>{
+            return gametitle.title
+        })
+    )]
+
+    const freeGames = players.flatMap((player)=>{
+        return player.games
+    }).filter((game)=>{
+        return game.free
+    }).map((gamename)=>{
+        return gamename.title
+    })
+
+    const uniqueFreeGames = [...new Set(
+        players.flatMap((player)=>{
+            return player.games
+        }).filter((game)=>{
+            return game.free
+        }).map((gamename)=>{
+            return gamename.title
+        })
+    )]
+
+    const genres = [...new Set(
+        players.flatMap((player)=>{
+            return player.games
+        }).map((genre)=>{
+            return genre.genre
+        })
+    )]
+    return {
+        totalPlayers: database.players.length,
+        premiumPlayers,
+        allGames,
+        uniqueGames,
+        freeGames,
+        uniqueFreeGames,
+        genres
+    }
+}
+console.log(generateLibrary(database))
+
+
+
