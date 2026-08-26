0eee6a689406a3364f12ab295f3075633676edd0
Mahdi <omohammadi376@gmail.com>
2026-08-26 21:39:42 +0330

    Add callbacks and functions

--- Asynchronous JavaScript/Callbacks/main.js
+++ Asynchronous JavaScript/Callbacks/main.js
@@ -1,1 +1,19 @@
+function greetUser(username, callback) {
+    console.log(`Welcome ${username}`)
+    callback()
+}
+greetUser('Mahdi', () =>{
+    console.log('Open Dashboard')
+})
+
+
+
+
+function calculateLevel(level, callback) {
+    callback(level + 10)
+}
+
+calculateLevel(55, (newLevel) => {
+    console.log(`New level: ${newLevel}`);
+});
 
