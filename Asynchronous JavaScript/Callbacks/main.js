432af1387f4c3cbe300cb4f15871fb2c43230e10
Mahdi <omohammadi376@gmail.com>
2026-08-27 18:03:18 +0330

    Add more functions to undrestand callbacks better

--- Asynchronous JavaScript/Callbacks/main.js
+++ Asynchronous JavaScript/Callbacks/main.js
@@ -8,7 +8,6 @@ greetUser('Mahdi', () =>{
 
 
 
-
 function calculateLevel(level, callback) {
     callback(level + 10)
 }
@@ -17,3 +16,30 @@ calculateLevel(55, (newLevel) => {
     console.log(`New level: ${newLevel}`);
 });
 
+
+
+function loginPlayer(username, callback) {
+    const player = {
+        username: username,
+        loggedIn: true
+    };
+    callback(player);
+}
+
+loginPlayer("Mahdi", (player) => {
+    console.log(`${player.username} logged in: ${player.loggedIn}`);
+});
+
+
+
+function processPlayer(username, callback) {
+    const player = {
+        username: username,
+        level: 10
+    };
+
+    callback(player);
+}
+processPlayer("Jack", (result) => {
+    console.log(`Welcome ${result.username}`);
+});
