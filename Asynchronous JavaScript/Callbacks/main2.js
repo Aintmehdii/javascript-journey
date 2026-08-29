94eeab0f849cc6ffdd4da2aecb0dd35423cc5fb5
Mahdi <omohammadi376@gmail.com>
2026-08-29 13:25:12 +0330

    Implement player management functions with callbacks

---
+++ Asynchronous JavaScript/Callbacks/main2.js
@@ -0,0 +1,61 @@
+function greet(username, callback) {
+    console.log(`Hello ${username}`)
+    callback()
+}
+greet('Mahdi', () => {
+    console.log('Welcome to the game!')
+})
+
+
+
+function addLevel(level, callback) {
+    callback(level += 10)
+}
+addLevel(55, (newLevel) => {
+    console.log(`New level: ${newLevel}`)
+})
+
+
+
+function createPlayer(username, level, callback) {
+    Player = {
+        username: username,
+        level: level
+    }
+
+    callback(Player)
+}
+createPlayer('Mahdi', 55, (Player)=>{
+    console.log(`${Player.username} | level: ${Player.level}`)
+
+
+
+function processPlayer(username, callback) {
+    Player = {
+        username: username,
+        level: 1
+    }
+    callback(Player)
+}
+processPlayer('Mahdi', (Player)=>{
+    console.log(`Welcome ${Player.username}`)
+})
+processPlayer('Mahdi', (Player)=>{
+    console.log(`Loading level ${Player.level}`)
+})
+
+
+
+
+function getPlayer(username, level, callback) {
+    Player = {
+        username: username,
+        level: level
+    }
+    callback(Player)
+}
+function showPlayer(Player) {
+    console.log(`${Player.username} | ${Player.level}`)
+}
+
+getPlayer('Mahdi', 55, showPlayer)
